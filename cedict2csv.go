07a56ecf95a7aae4ebc9811a7a87f3278061861c
sylvainv <sylvain.verly@gmail.com>
2018-03-11 18:18:15 +0800

    Add cedict 2 csv script

---
+++ cedict2csv.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+  "github.com/hermanschaaf/cedict"
+  "bufio"
+  "os"
+  "encoding/csv"
+  "flag"
+  "log"
+)
+
+func main () {
+  cedictFileName := flag.String("cedict", nil, "Cedict file to parse")
+  outputFileName := flag.String("output", nil, "Output csv")
+  flag.Parse()
+
+  if (flag.NFlag() != 2) {
+    flag.PrintDefaults()
+    return
+  }
+
+  cedictFile, cedictErr := os.Open(*cedictFileName)
+  checkError("Could not read cedict file", cedictErr)
+  defer cedictFile.Close()
+
+  cedictReader := cedict.New(bufio.NewReader(cedictFile))
+
+  csvFile, csvErr := os.Create(*outputFileName)
+  checkError("Could not create output file", csvErr)
+  defer csvFile.Close()
+
+  writer := csv.NewWriter(csvFile)
+  defer writer.Flush()
+
+  var data = []string{"simplified", "traditional", "pinyin", "pinyin_with_tones", "pinyin_no_tones", "definition1", "definition2"}
+  err := writer.Write(data)
+  checkError("Could not write to file", err)
+
+  for {
+    err = cedictReader.NextEntry()
+    if err != nil {
+        break
+    }
+    entry := cedictReader.Entry()
+
+    data = append([]string{
+        entry.Simplified,
+        entry.Traditional,
+        entry.Pinyin,
+        entry.PinyinWithTones,
+        entry.PinyinNoTones,
+      },
+      entry.Definitions[:]...
+    )
+    err = writer.Write(data)
+    checkError("Could not write to file", err)
+  }
+}
+
+func checkError(message string, err error) {
+    if err != nil {
+        log.Fatal(message, err)
+        panic(err.Error())
+    }
+}
+
+
